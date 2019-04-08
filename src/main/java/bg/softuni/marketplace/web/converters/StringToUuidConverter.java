f5500be53ff3a2dad29a8ef8af6a406a9b174bc0
MartinBG <MartinBG@abv.bg>
2019-04-08 16:42:23 +0300

    String to UUID converter

---
+++ src/main/java/bg/softuni/marketplace/web/converters/StringToUuidConverter.java
@@ -0,0 +1,16 @@
+package bg.softuni.marketplace.web.converters;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Component
+public class StringToUuidConverter implements Converter<String, UUID> {
+
+    @Override
+    public UUID convert(String id) {
+        return UUID.fromString(Objects.requireNonNull(id));
+    }
+}
