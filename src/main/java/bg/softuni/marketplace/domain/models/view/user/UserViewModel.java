035820244b66a2c8fe5926faf74a48ac2d7a7383
MartinBG <MartinBG@abv.bg>
2019-04-08 15:14:26 +0300

    User view model

---
+++ src/main/java/bg/softuni/marketplace/domain/models/view/user/UserViewModel.java
@@ -0,0 +1,27 @@
+package bg.softuni.marketplace.domain.models.view.user;
+
+import bg.softuni.marketplace.domain.api.Viewable;
+import bg.softuni.marketplace.domain.entities.User;
+import bg.softuni.marketplace.domain.enums.Authority;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Getter
+@EqualsAndHashCode(of = {"id"})
+@NoArgsConstructor
+@AllArgsConstructor
+public final class UserViewModel implements Viewable<User>, Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private UUID id;
+
+    private String username;
+
+    private String email;
+
+    @Setter
+    private Authority authority;
+}
