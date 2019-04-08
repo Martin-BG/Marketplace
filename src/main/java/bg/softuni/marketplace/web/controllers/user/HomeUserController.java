2db62d8b8706d7a8c8a609f6aee56210028c101f
MartinBG <MartinBG@abv.bg>
2019-04-08 16:56:02 +0300

    Home controller

---
+++ src/main/java/bg/softuni/marketplace/web/controllers/user/HomeUserController.java
@@ -0,0 +1,20 @@
+package bg.softuni.marketplace.web.controllers.user;
+
+import bg.softuni.marketplace.config.WebConfig;
+import bg.softuni.marketplace.web.annotations.Layout;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Layout
+@Controller
+@RequestMapping(WebConfig.URL_USER_HOME)
+public class HomeUserController {
+
+    private static final String VIEW_HOME = "user/home";
+
+    @GetMapping
+    public String get() {
+        return VIEW_HOME;
+    }
+}
