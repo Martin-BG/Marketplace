2b7a60b9b311bde22e6d6e44c20cc85301e40c71
MartinBG <MartinBG@abv.bg>
2019-04-08 14:47:21 +0300

    Role entity

---
+++ src/main/java/bg/softuni/marketplace/domain/entities/Role.java
@@ -0,0 +1,32 @@
+package bg.softuni.marketplace.domain.entities;
+
+import bg.softuni.marketplace.domain.converters.AuthorityConverter;
+import bg.softuni.marketplace.domain.enums.Authority;
+import bg.softuni.marketplace.domain.validation.annotations.composite.role.ValidRoleAuthority;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role extends BaseLongEntity implements GrantedAuthority {
+
+    private static final long serialVersionUID = 1L;
+
+    @ValidRoleAuthority
+    @Convert(converter = AuthorityConverter.class)
+    @Column(unique = true, nullable = false, length = ValidRoleAuthority.MAX_LENGTH)
+    private Authority authority;
+
+    @Override
+    public String getAuthority() {
+        return authority.asRole();
+    }
+}
