a23c159144ba33a154d2d4e8565435787423082b
MartinBG <MartinBG@abv.bg>
2019-04-08 14:43:46 +0300

    user validation annotations

---
+++ src/main/java/bg/softuni/marketplace/domain/validation/annotations/composite/user/ValidUserEncryptedPassword.java
@@ -0,0 +1,34 @@
+package bg.softuni.marketplace.domain.validation.annotations.composite.user;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Cannot be null, length is {@value MIN_LENGTH}-{@value MAX_LENGTH}
+ */
+
+@NotNull
+@Size(min = ValidUserEncryptedPassword.MIN_LENGTH, max = ValidUserEncryptedPassword.MAX_LENGTH)
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER})
+@Retention(RUNTIME)
+@Constraint(validatedBy = {})
+@Documented
+public @interface ValidUserEncryptedPassword {
+
+    int MIN_LENGTH = 60;
+    int MAX_LENGTH = 60;
+
+    String message() default "";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
