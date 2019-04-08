befd5ea0388588231a169fda0ff1285e13a1661c
MartinBG <MartinBG@abv.bg>
2019-04-08 14:58:05 +0300

    User Repository

---
+++ src/main/java/bg/softuni/marketplace/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package bg.softuni.marketplace.repository;
+
+import bg.softuni.marketplace.domain.entities.User;
+import bg.softuni.marketplace.domain.validation.annotations.composite.user.ValidUserEmail;
+import bg.softuni.marketplace.domain.validation.annotations.composite.user.ValidUserUsername;
+import org.springframework.stereotype.Repository;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Validated
+@Repository
+public interface UserRepository extends GenericRepository<User, UUID> {
+
+    Optional<User> findUserEager(@ValidUserUsername String username);
+
+    long countByUsername(@ValidUserUsername String username);
+
+    long countByEmail(@ValidUserEmail String email);
+}
