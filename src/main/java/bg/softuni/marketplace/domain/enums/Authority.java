3039de2c17cb346a016c151179e79a5da5e5610c
MartinBG <MartinBG@abv.bg>
2019-04-08 14:45:39 +0300

    Authority enum

---
+++ src/main/java/bg/softuni/marketplace/domain/enums/Authority.java
@@ -0,0 +1,31 @@
+package bg.softuni.marketplace.domain.enums;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum Authority {
+    ROOT,
+    ADMIN,
+    TRADER,
+    USER;
+
+    private static final Map<String, Authority> STRING_TO_ENUM = Stream.of(Authority.values())
+            .collect(Collectors.toUnmodifiableMap(Authority::asRole, authority -> authority));
+
+    private static final String ROLE_PREFIX = "ROLE_";
+
+    private final String role;
+
+    Authority() {
+        this.role = ROLE_PREFIX + name();
+    }
+
+    public static Authority fromRole(String role) {
+        return role == null ? null : STRING_TO_ENUM.get(role);
+    }
+
+    public String asRole() {
+        return role;
+    }
+}
