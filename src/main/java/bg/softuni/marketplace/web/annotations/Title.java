12f8771fab7e458d10deb801a9c8335e6cec3460
MartinBG <MartinBG@abv.bg>
2019-04-11 05:55:11 +0300

    Title interceptor and annotation

---
+++ src/main/java/bg/softuni/marketplace/web/annotations/Title.java
@@ -0,0 +1,49 @@
+package bg.softuni.marketplace.web.annotations;
+
+import bg.softuni.marketplace.web.interceptors.TitleInterceptor;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.*;
+
+/**
+ * Page title suffix code (defined in *.properties file) specifier (default) or replace default title.
+ * <p>
+ * Can be placed on Controller methods.
+ * <p>
+ * Title is modified by {@link TitleInterceptor}
+ * <hr>
+ * Sample html:
+ * <pre>{@code <head>
+ *     ...
+ *     <title th:text="${title}"/>
+ *     ...
+ * </head>}</pre>
+ * Sample controller method:
+ * <pre>{@code @Title("nav.login")
+ * @GetMapping
+ * public String get() {
+ *     return USER_LOGIN;
+ * }}</pre>
+ * Sample messages.properties:
+ * <pre>{@code application.title=My App
+ * title.login=Login}</pre>
+ * Result title: <em>My App - Login</em>
+ *
+ * <pre>{@code @Title(title="nav.login", append=false)}</pre>
+ * Result title: "Login"
+ * <hr>
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
+public @interface Title {
+
+    @AliasFor("title")
+    String value() default "";
+
+    @AliasFor("value")
+    String title() default "";
+
+    boolean append() default true;
+}
---
+++ src/main/java/bg/softuni/marketplace/web/interceptors/TitleInterceptor.java
@@ -0,0 +1,100 @@
+package bg.softuni.marketplace.web.interceptors;
+
+import bg.softuni.marketplace.web.annotations.Title;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.util.Assert;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+/**
+ * Modifies page title according to {@link Title} annotation.
+ * <ul>Default values (can be changed during interceptor's instance build):
+ * <li>Title code (as defined in *.properties text file): {@value #DEFAULT_TITLE_CODE}</li>
+ * <li>View attribute name in Thymeleaf template: {@value #DEFAULT_TITLE_ATTRIBUTE_NAME}</li>
+ * </ul>
+ * <hr>
+ * Sample html:
+ * <pre>{@code <head>
+ *     ...
+ *     <title th:text="${title}"/>
+ *     ...
+ * </head>}</pre>
+ */
+
+public final class TitleInterceptor extends HandlerInterceptorAdapter {
+
+    private static final String DEFAULT_TITLE_CODE = "application.title";
+    private static final String DEFAULT_TITLE_ATTRIBUTE_NAME = "title";
+    private final MessageSource messageSource;
+    private String titleCode;
+    private String titleAttribute;
+
+    private TitleInterceptor(MessageSource messageSource) {
+        this.messageSource = messageSource;
+        titleCode = DEFAULT_TITLE_CODE;
+        titleAttribute = DEFAULT_TITLE_ATTRIBUTE_NAME;
+    }
+
+    public static TitleInterceptor.Builder builder(MessageSource messageSource) {
+        return new TitleInterceptor.Builder(messageSource);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response,
+                           Object handler,
+                           ModelAndView modelAndView) {
+        if (handler instanceof HandlerMethod) {
+            Locale locale = LocaleContextHolder.getLocale();
+            String title = messageSource.getMessage(titleCode, null, locale);
+
+            Title titleAnnotation = ((HandlerMethod) handler).getMethodAnnotation(Title.class);
+
+            if (titleAnnotation != null) {
+                String newTitle = messageSource.getMessage(titleAnnotation.title(), null, locale);
+                if (titleAnnotation.append()) {
+                    title = title + " - " + newTitle;
+                } else {
+                    title = newTitle;
+                }
+            }
+
+            modelAndView.addObject(titleAttribute, title);
+        }
+    }
+
+    public static final class Builder {
+        private TitleInterceptor toBuild;
+
+        private Builder(MessageSource messageSource) {
+            toBuild = new TitleInterceptor(messageSource);
+        }
+
+        public Builder withTitleCode(String titleCode) {
+            toBuild.titleCode = titleCode;
+            return this;
+        }
+
+        public Builder withTitleAttribute(String titleAttribute) {
+            toBuild.titleAttribute = titleAttribute;
+            return this;
+        }
+
+        public TitleInterceptor build() {
+            Assert.notNull(toBuild, "Reuse of builder is not allowed");
+            Assert.notNull(toBuild.messageSource, "MessageSource cannot be null");
+            Assert.hasLength(toBuild.titleCode, "Title code cannot be empty or null");
+            Assert.hasLength(toBuild.titleAttribute, "Title attribute name cannot be empty or null");
+
+            TitleInterceptor built = toBuild;
+            toBuild = null;
+            return built;
+        }
+    }
+}
