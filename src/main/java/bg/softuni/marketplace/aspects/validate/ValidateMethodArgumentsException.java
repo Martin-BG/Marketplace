65767a7f39936f59588578960c41658fe21cf54e
MartinBG <MartinBG@abv.bg>
2019-04-17 16:33:44 +0300

    access changed to package-private

--- src/main/java/bg/softuni/marketplace/aspects/validate/ValidateMethodArgumentsException.java
+++ src/main/java/bg/softuni/marketplace/aspects/validate/ValidateMethodArgumentsException.java
@@ -1,11 +1,11 @@
 package bg.softuni.marketplace.aspects.validate;
 
-public class ValidateMethodArgumentsException extends RuntimeException {
+class ValidateMethodArgumentsException extends RuntimeException {
 
     /**
      * Lightweight exception - stacktrace disabled, suppression enabled
      */
-    public ValidateMethodArgumentsException(String message) {
+    ValidateMethodArgumentsException(String message) {
         super(message, null, true, false);
     }
 }
