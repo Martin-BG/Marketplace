d40375e11690ebf93f37de683acd0deb1ade0e8f
MartinBG <MartinBG@abv.bg>
2019-04-17 21:08:58 +0300

    refactoring: - @Title moved to class level - UserRegisterBindingModel instantiation moved to @ModelAttribute private method - add @OnError to post method

--- src/main/java/bg/softuni/marketplace/web/controllers/user/RegisterUserController.java
+++ src/main/java/bg/softuni/marketplace/web/controllers/user/RegisterUserController.java
@@ -1,13 +1,14 @@
 package bg.softuni.marketplace.web.controllers.user;
 
+import bg.softuni.marketplace.aspects.onerror.OnError;
 import bg.softuni.marketplace.config.WebConfig;
 import bg.softuni.marketplace.domain.models.binding.user.UserRegisterBindingModel;
 import bg.softuni.marketplace.service.UserService;
 import bg.softuni.marketplace.web.annotations.Layout;
 import bg.softuni.marketplace.web.annotations.Title;
 import bg.softuni.marketplace.web.controllers.BaseController;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Layout
+@Title("nav.register")
+@RequiredArgsConstructor
 @Controller
 @RequestMapping(WebConfig.URL_USER_REGISTER)
 public class RegisterUserController extends BaseController {
@@ -27,31 +30,26 @@ public class RegisterUserController extends BaseController {
 
     private final UserService userService;
 
-    public RegisterUserController(UserService userService) {
-        this.userService = userService;
-    }
-
     @Override
     protected List<String> getUnmodifiedTextFieldsList() {
         return List.of("password", "confirmPassword");
     }
 
-    @GetMapping
-    @Title("nav.register")
-    public String get(Model model) {
-        model.addAttribute(USER_ATTRIBUTE_NAME, new UserRegisterBindingModel());
+    @ModelAttribute(USER_ATTRIBUTE_NAME)
+    private UserRegisterBindingModel userRegisterBindingModel() {
+        return new UserRegisterBindingModel();
+    }
 
+    @GetMapping
+    public String get() {
         return VIEW_REGISTER;
     }
 
     @PostMapping
-    public String post(@ModelAttribute(USER_ATTRIBUTE_NAME) UserRegisterBindingModel user, Errors errors) {
+    @OnError(view = VIEW_REGISTER, catchException = true)
+    public String post(@ModelAttribute(USER_ATTRIBUTE_NAME) UserRegisterBindingModel user,
+                       Errors errors) {
         userService.registerUser(user, errors);
-
-        if (errors.hasErrors()) {
-            return VIEW_REGISTER;
-        }
-
         return redirect(WebConfig.URL_USER_LOGIN + "?register");
     }
 }
