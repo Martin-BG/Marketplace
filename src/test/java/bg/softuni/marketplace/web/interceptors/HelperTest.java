3242b3191a17a4a5f81247d61ceb3693ab114fb2
MartinBG <MartinBG@abv.bg>
2019-04-18 04:22:59 +0300

    prettifying

--- src/test/java/bg/softuni/marketplace/web/interceptors/HelperTest.java
+++ src/test/java/bg/softuni/marketplace/web/interceptors/HelperTest.java
@@ -1,39 +1,58 @@
 package bg.softuni.marketplace.web.interceptors;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("interceptors/Helper.class")
 class HelperTest {
 
-    @ParameterizedTest
-    @ValueSource(strings = {"redirect:someUrl", "redirect:/", "redirect:"})
-    void isRedirectOrForward_withRedirect_isTrue(String url) {
-        assertTrue(Helper.isRedirectOrForward(url));
-    }
+    @Nested
+    @DisplayName("isRedirectOrForward(String url) method")
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class isRedirectOrForwardTests {
 
-    @ParameterizedTest
-    @ValueSource(strings = {"forward:someUrl", "forward:/", "forward:"})
-    void isRedirectOrForward_withForward_isTrue(String url) {
-        assertTrue(Helper.isRedirectOrForward(url));
-    }
+        @DisplayName("redirect with valid URLs -> true")
+        @ParameterizedTest
+        @ValueSource(strings = {"redirect:someUrl", "redirect:/", "redirect:"})
+        void isRedirectOrForward_withRedirect_isTrue(String url) {
+            assertTrue(Helper.isRedirectOrForward(url));
+        }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"Forward:/", "Redirect:/", "FORWARD:/", "REDIRECT:/"})
-    void isRedirectOrForward_withNonLowercaseLetters_isFalse(String url) {
-        assertFalse(Helper.isRedirectOrForward(url));
-    }
+        @DisplayName("forward with valid URLs -> true")
+        @ParameterizedTest
+        @ValueSource(strings = {"forward:someUrl", "forward:/", "forward:"})
+        void isRedirectOrForward_withForward_isTrue(String url) {
+            assertTrue(Helper.isRedirectOrForward(url));
+        }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"/someUrl", "/user/home", "/forward", "/redirect"})
-    void isRedirectOrForward_withUrl_isFalse(String url) {
-        assertFalse(Helper.isRedirectOrForward(url));
-    }
+        @DisplayName("forward and redirect with uppercase or mixed letters -> false")
+        @ParameterizedTest
+        @ValueSource(strings = {"Forward:/", "Redirect:/", "FORWARD:/", "REDIRECT:/"})
+        void isRedirectOrForward_withNonLowercaseLetters_isFalse(String url) {
+            assertFalse(Helper.isRedirectOrForward(url));
+        }
 
-    @Test
-    void isRedirectOrForward_withNull_throwsNPE() {
-        assertThrows(NullPointerException.class, () -> Helper.isRedirectOrForward(null));
+        @DisplayName("simple URLs -> false")
+        @ParameterizedTest
+        @EmptySource
+        @ValueSource(strings = {"/someUrl", "/user/home", "/forward", "/redirect"})
+        void isRedirectOrForward_withUrl_isFalse(String url) {
+            assertFalse(Helper.isRedirectOrForward(url));
+        }
+
+        @DisplayName("with null -> NullPointerException")
+        @ParameterizedTest
+        @NullSource
+        void isRedirectOrForward_withNull_throwsNPE(String url) {
+            assertThrows(NullPointerException.class, () -> Helper.isRedirectOrForward(url));
+        }
     }
 }
