95fcf541ff1178d37cbd4c06718378259c9977dd
MartinBG <MartinBG@abv.bg>
2019-04-19 20:05:10 +0300

    add new validation group: GroupThree.java

--- src/main/java/bg/softuni/marketplace/domain/validation/groups/AllGroups.java
+++ src/main/java/bg/softuni/marketplace/domain/validation/groups/AllGroups.java
@@ -3,6 +3,6 @@ package bg.softuni.marketplace.domain.validation.groups;
 import javax.validation.GroupSequence;
 import javax.validation.groups.Default;
 
-@GroupSequence({Default.class, GroupOne.class, GroupTwo.class})
+@GroupSequence({Default.class, GroupOne.class, GroupTwo.class, GroupThree.class})
 public interface AllGroups {
 }
