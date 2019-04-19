8b4f40a2f1bb6ece4ac8e07b47b21c4932e9536b
MartinBG <MartinBG@abv.bg>
2019-04-19 23:23:44 +0300

    rearranged constraints on fields to be in order of evaluation, add validation to prevent ROOT user edit

--- src/main/java/bg/softuni/marketplace/domain/models/binding/user/UserRoleBindingModel.java
+++ src/main/java/bg/softuni/marketplace/domain/models/binding/user/UserRoleBindingModel.java
@@ -5,9 +5,10 @@ import bg.softuni.marketplace.domain.entities.User;
 import bg.softuni.marketplace.domain.enums.Authority;
 import bg.softuni.marketplace.domain.validation.annotations.composite.role.ValidAuthority;
 import bg.softuni.marketplace.domain.validation.annotations.composite.user.ValidUserUsername;
+import bg.softuni.marketplace.domain.validation.annotations.spel.SpELAssert;
 import bg.softuni.marketplace.domain.validation.groups.GroupOne;
 import bg.softuni.marketplace.domain.validation.groups.GroupTwo;
-import cz.jirutka.validator.spring.SpELAssert;
+import bg.softuni.marketplace.domain.validation.helpers.UserValidationHelper;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,13 +24,16 @@ public class UserRoleBindingModel implements Bindable<User>, Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @SpELAssert(message = "{user.update.username.not-found}", groups = GroupTwo.class,
-            value = "@userRepository.countByUsername(#this) == 1L")
     @ValidUserUsername
+    @SpELAssert(message = "{user.update.username.not-found}", groups = GroupOne.class,
+            value = "@userRepository.countByUsername(#this) == 1L")
+    @SpELAssert(message = "{user.update.username.not-root}", groups = GroupTwo.class,
+            value = "#isNotRoot(@userRepository.findUserEager(#this).get())",
+            helpers = UserValidationHelper.class)
     private String username;
 
+    @ValidAuthority
     @SpELAssert(message = "{user.update.role.root}", groups = GroupOne.class,
             value = "#this != T(bg.softuni.marketplace.domain.enums.Authority).ROOT")
-    @ValidAuthority
     private Authority authority;
 }
