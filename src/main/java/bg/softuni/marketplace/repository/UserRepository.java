c3d6f47f9b37ef27ece18a51bc1332245d24e160
MartinBG <MartinBG@abv.bg>
2019-04-23 04:22:02 +0300

    refactor UserRepository - custom @Query methods with "readonly" @QueryHint: - boolean hasAuthority(username, authority) - long countByUsername -> boolean hasUsername - long countByEmail -> boolean hasEmail - Javadoc

--- src/main/java/bg/softuni/marketplace/repository/UserRepository.java
+++ src/main/java/bg/softuni/marketplace/repository/UserRepository.java
@@ -1,23 +1,72 @@
 package bg.softuni.marketplace.repository;
 
 import bg.softuni.marketplace.domain.entities.User;
+import bg.softuni.marketplace.domain.enums.Authority;
 import bg.softuni.marketplace.domain.validation.annotations.composite.user.ValidUserEmail;
 import bg.softuni.marketplace.domain.validation.annotations.composite.user.ValidUserUsername;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 import org.springframework.validation.annotation.Validated;
 
+import javax.persistence.QueryHint;
+import javax.validation.constraints.NotNull;
 import java.util.Optional;
 import java.util.UUID;
+
+import static org.hibernate.jpa.QueryHints.HINT_READONLY;
 
 @Validated
 @Repository
 public interface UserRepository extends GenericRepository<User, UUID> {
 
+    /**
+     * Find {@link User} with all roles.
+     * Uses a {@link javax.persistence.NamedQuery @NamedQuery} defined in {@link User}.
+     *
+     * @param username username
+     * @return {@link Optional}<{@link User}>
+     * @see User
+     */
     Optional<User> findUserEager(@ValidUserUsername String username);
 
-    long countByUsername(@ValidUserUsername String username);
+    /**
+     * Check if username exists and has specified authority.
+     * Uses a custom {@link Query @Query}.
+     *
+     * @param username  {@link String}
+     * @param authority {@link Authority}
+     * @return {@code true} - username with authority found,
+     * {@code false} - either username not found or missing authority
+     */
+    @Query("SELECT (COUNT(u) > 0) FROM User u LEFT JOIN u.authorities AS a " +
+            "WHERE u.username = :username AND a.authority = :authority")
+    @QueryHints(@QueryHint(name = HINT_READONLY, value = "true"))
+    boolean hasAuthority(@ValidUserUsername String username, @NotNull Authority authority);
 
-    long countByEmail(@ValidUserEmail String email);
+    /**
+     * Check if {@link User} with specified username exists.
+     * Uses a custom {@link Query @Query}.
+     *
+     * @param username {@link String}
+     * @return {@code true} - {@link User} with username found,
+     * {@code false} - no {@link User} with username found.
+     */
+    @Query("SELECT (COUNT(u) > 0) FROM User u WHERE u.username = :username")
+    @QueryHints(@QueryHint(name = HINT_READONLY, value = "true"))
+    boolean hasUsername(@ValidUserUsername String username);
+
+    /**
+     * Check if {@link User} with specified email exists.
+     * Uses a custom {@link Query @Query}.
+     *
+     * @param email {@link String}
+     * @return {@code true} - {@link User} with email found,
+     * {@code false} - no {@link User} with email found.
+     */
+    @Query("SELECT (COUNT(u) > 0) FROM User u WHERE u.email = :email")
+    @QueryHints(@QueryHint(name = HINT_READONLY, value = "true"))
+    boolean hasEmail(@ValidUserEmail String email);
 
     void deleteByUsername(@ValidUserUsername String username);
 }
