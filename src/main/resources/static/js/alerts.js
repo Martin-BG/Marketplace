119dfeddd036fe43b2a573df220f215b8cf78638
MartinBG <MartinBG@abv.bg>
2019-04-29 18:58:36 +0300

    alerts: add fadeIn, increased fadeOut time (0.3->0.5s), increased errorTimeout (7s->10s)

--- src/main/resources/static/js/alerts.js
+++ src/main/resources/static/js/alerts.js
@@ -1,10 +1,15 @@
 $(document).ready(TriggerAlertClose());
 
 function TriggerAlertClose() {
-    const fadeOutTime = 300;
+    const fadeOutTime = 500;
+    const fadeInTime = 500;
     const infoTimeout = 3000;
     const successTimeout = 5000;
-    const errorTimeout = 7000;
+    const errorTimeout = 10000;
+
+    $('.alert-info').hide().fadeIn(fadeInTime);
+    $('.alert-success').hide().fadeIn(fadeInTime);
+    $('.alert-danger').hide().fadeIn(fadeInTime);
 
     $('.alert').click(function () {
         $(this)
