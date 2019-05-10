9b4cd1cd47f0ad20522d0400c17a5d79337ec909
MartinBG <MartinBG@abv.bg>
2019-05-10 15:54:04 +0300

    javadoc

--- src/main/java/bg/softuni/marketplace/service/helpers/UserServiceHelper.java
+++ src/main/java/bg/softuni/marketplace/service/helpers/UserServiceHelper.java
@@ -5,16 +5,45 @@ import bg.softuni.marketplace.domain.entities.User;
 import bg.softuni.marketplace.domain.enums.Authority;
 import bg.softuni.marketplace.domain.models.binding.user.UserRegisterBindingModel;
 import bg.softuni.marketplace.domain.models.view.user.UserViewModel;
+import bg.softuni.marketplace.service.UserService;
 
 import java.util.List;
 
+/**
+ * {@link UserService} helper methods interface.
+ */
 public interface UserServiceHelper {
 
+    /**
+     * Maps {@link User} to {@link UserViewModel}.
+     *
+     * @param user the user
+     * @return the user view model
+     */
     UserViewModel mapUserToViewModel(User user);
 
+    /**
+     * Update role for user.
+     *
+     * @param user      the user
+     * @param authority the authority
+     */
     void updateRoleForUser(User user, Authority authority);
 
+    /**
+     * Gets {@link User} from {@link UserRegisterBindingModel} model.
+     *
+     * @param bindingModel the {@link UserRegisterBindingModel} model
+     * @param isRoot       is this user ROOT (the first user)
+     * @return the {@link User} from model
+     */
     User getUserFromModel(UserRegisterBindingModel bindingModel, boolean isRoot);
 
+    /**
+     * Gets default roles for new user.
+     *
+     * @param isRoot the user is root
+     * @return the roles for user
+     */
     List<Role> getRolesForUser(boolean isRoot);
 }
