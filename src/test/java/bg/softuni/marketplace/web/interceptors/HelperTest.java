d0d72372009aa74519ba90544822bc7c1afc381b
MartinBG <MartinBG@abv.bg>
2019-09-10 13:47:25 +0300

    update display name

--- src/test/java/bg/softuni/marketplace/web/interceptors/HelperTest.java
+++ src/test/java/bg/softuni/marketplace/web/interceptors/HelperTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayName("interceptors/Helper.class")
+@DisplayName("web/interceptors/Helper.class")
 class HelperTest {
 
     @Nested
