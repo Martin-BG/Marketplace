715479d35757aa686fae9bbeb011f8b84304d364
MartinBG <MartinBG@abv.bg>
2019-09-21 05:13:39 +0300

    logging - lazy argument resolving

--- src/main/java/bg/softuni/marketplace/web/handlers/AccessDeniedHandlerImpl.java
+++ src/main/java/bg/softuni/marketplace/web/handlers/AccessDeniedHandlerImpl.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -21,13 +20,13 @@ public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request,
                        HttpServletResponse response,
-                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                       AccessDeniedException accessDeniedException) throws IOException {
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
         if (auth != null) {
-            log.log(Level.INFO, String.format(
-                    "User: %s attempted to access the protected URL: %s",
+            log.log(Level.WARNING, () -> String.format(
+                    "Unauthorized access: {%s} attempted to access {%s}",
                     auth.getName(), request.getRequestURI()));
         }
 
