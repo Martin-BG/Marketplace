3485f680a5ba596d9cb4265c834f039933309a61
MartinBG <MartinBG@abv.bg>
2023-12-03 23:38:11 +0200

    add few basic tests to ensure that method argument validation works

---
+++ src/test/java/bg/softuni/marketplace/repository/UserRepositoryTest.java
@@ -0,0 +1,67 @@
+package bg.softuni.marketplace.repository;
+
+import bg.softuni.marketplace.domain.validation.annotations.composite.user.ValidUserUsername;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import jakarta.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
+
+@SpringBootTest
+@DisplayName("repository/UserRepository.class")
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private MessageSource messageSource;
+
+    @Nested
+    @DisplayName("findUserByUsername(String username) method")
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class findUserByUsernameTests {
+
+        private final String TOO_LONG_USER_NAME = "Too long user name is used here...";
+        private final String VALID_USER_NAME = "John Smith";
+        private final String blankMessage = messageSource.getMessage("user.username.blank", null, LocaleContextHolder.getLocale());
+        private final String lengthMessage = messageSource.getMessage("user.username.length", null, LocaleContextHolder.getLocale())
+                .replaceAll("\\{min}", Integer.toString(ValidUserUsername.MIN_LENGTH))
+                .replaceAll("\\{max}", Integer.toString(ValidUserUsername.MAX_LENGTH));
+
+        @DisplayName("valid username -> not throwing")
+        @Test
+        void findUserByUsernameWithValidString() {
+            assertThatNoException()
+                    .isThrownBy(() -> userRepository.findUserByUsername(VALID_USER_NAME));
+        }
+
+        @DisplayName("null username -> throws")
+        @Test
+        void findUserByUsernameWithNull() {
+            assertThatExceptionOfType(ConstraintViolationException.class)
+                    .isThrownBy(() -> userRepository.findUserByUsername(null))
+                    .withMessageContainingAll(blankMessage);
+        }
+
+        @DisplayName("empty username -> throws")
+        @Test
+        void findUserByUsernameWithEmptyString() {
+            assertThatExceptionOfType(ConstraintViolationException.class)
+                    .isThrownBy(() -> userRepository.findUserByUsername(""))
+                    .withMessageContainingAll(blankMessage, lengthMessage);
+        }
+
+        @DisplayName("too long username -> throws")
+        @Test
+        void findUserByUsernameWithTooLongString() {
+            assertThatExceptionOfType(ConstraintViolationException.class)
+                    .isThrownBy(() -> userRepository.findUserByUsername(TOO_LONG_USER_NAME))
+                    .withMessageContaining(lengthMessage);
+        }
+    }
+}
