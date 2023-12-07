31793f8a93f85bd0129341c8ebe051d45d5300b2
MartinBG <MartinBG@abv.bg>
2023-12-08 01:51:12 +0200

    update UUID generation to Hibernate 6+ style

--- src/main/java/bg/softuni/marketplace/domain/entities/BaseUuidEntity.java
+++ src/main/java/bg/softuni/marketplace/domain/entities/BaseUuidEntity.java
@@ -4,7 +4,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UuidGenerator;
 
 import jakarta.persistence.*;
 
@@ -40,8 +40,8 @@ class BaseUuidEntity extends BaseEntity<UUID> {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @UuidGenerator(style = UuidGenerator.Style.RANDOM)
     @Column(unique = true, nullable = false, insertable = false, updatable = false, columnDefinition = "BINARY(16)")
     @Access(AccessType.PROPERTY)
     private UUID id;
